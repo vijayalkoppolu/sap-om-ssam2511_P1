44b7188e1f84c9e297b496a0255c1c3acb897ca5
Vijaya Lakshmi Koppolu <vlk@equinor.com>
2026-07-09 08:33:33 +0000

    SAP Note: 3644438

--- SAPAssetManager/Rules/WorkOrders/InspectionLot/SetUsage/InspectionLotSetUsageInspectionCodesPickerItems.js
+++ SAPAssetManager/Rules/WorkOrders/InspectionLot/SetUsage/InspectionLotSetUsageInspectionCodesPickerItems.js
@@ -1,9 +1,31 @@
 import InspectionLotSetUsageQueryOptions from './InspectionLotSetUsageQueryOptions';
 import InspectionCodesSortedPickerItems from '../../../InspectionCharacteristics/InspectionCodesSortedPickerItems';
-
+import libCom from '../../../Common/Library/CommonLibrary';
+import Logger from '../../../Log/Logger';
 export default async function InspectionLotSetUsageInspectionCodesPickerItems(context) {
     const queryOptions = await InspectionLotSetUsageQueryOptions(context);
-    const udPlanningPlant = context.binding.UDPlant;
+ //   const udPlanningPlant = context.binding.UDPlant;
+ // return InspectionCodesSortedPickerItems(context, queryOptions, udPlanningPlant);
+    let binding = context.binding;
+   let pageName = libCom.getStateVariable(context, 'FDCPreviousPage');
+   try {
+       let actionBinding = context.evaluateTargetPathForAPI('#Page:' + pageName).getClientData().ActionBinding;
+       if (actionBinding) {
+           binding = actionBinding;
+        }
+    } catch (error) {
+        Logger.error('InspectionLotSetUsageInspectionCodesPickerItems pageName does not exist ' + error);
+    }
+
+    let udPlanningPlant;
+    const inspectionLotId = binding.InspectionLot;
+    if (inspectionLotId) {
+        const lots = await context.read('/SAPAssetManager/Services/AssetManager.service', 'InspectionLots', [], `$filter=InspectionLot eq '${inspectionLotId}'&$select=UDPlant`);
+        if (lots && lots.length > 0) {
+            udPlanningPlant = lots.getItem(0).UDPlant;
+        }
+    }
 
     return InspectionCodesSortedPickerItems(context, queryOptions, udPlanningPlant);
+ 
 }
