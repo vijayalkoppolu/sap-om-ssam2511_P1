caf7a00ca1b0ab37fae1684e3b9c4cdf81dbd20c
Vijaya Lakshmi Koppolu <vlk@equinor.com>
2026-08-24 19:06:15 +0000

    NGE-129333

--- ZEquinorSSAM/Rules/Notifications/CreateUpdate/NotificationCreateUpdateOnPageLoad.js
+++ ZEquinorSSAM/Rules/Notifications/CreateUpdate/NotificationCreateUpdateOnPageLoad.js
@@ -178,7 +178,7 @@ export default async function NotificationCreateUpdateOnPageLoad(context) {
     });
 
     // Equinor: For local notifications in edit mode, keep Failure Effect Codegroup and Code read-only
-    if (!onCreate && ODataLibrary.isLocal(binding)) {
+    if (!onCreate && !ODataLibrary.isLocal(binding)) {
         try {
             const formCellContainer = context.getControl('FormCellContainer');
             formCellContainer.getControl('FailureEffectGroupListPicker').setEditable(false);
